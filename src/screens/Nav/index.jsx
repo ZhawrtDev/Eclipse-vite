197693529fb366ee9d876250412b6f3fbdb89007
Zhawrt <zhawrtsuporte@gmail.com>
2025-04-10 06:40:59 -0300

    testing

--- src/screens/Nav/index.jsx
+++ src/screens/Nav/index.jsx
@@ -117,7 +117,7 @@ function Nav() {
               <img src={logo} className="logo" />
             </div>
             <div className="text">
-              <p>ServerSid.</p>
+              <p>ServerSide.</p>
               <h1>ECLIPSE</h1>
             </div>
           </div>
